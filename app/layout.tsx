5ede7c9f284a58ad3170c72432a08b5fea3662cd
UOX ONE <uox.one@gmail.com>
2026-09-22 10:57:35 +0200

    feat : create Componnets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +29,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+
     </html>
   );
 }
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav style={{ background: "#0070f3", padding: "10px", color: "white" }}>
+            <h1>My Navbar</h1>
+        </nav>
+    );
+}
